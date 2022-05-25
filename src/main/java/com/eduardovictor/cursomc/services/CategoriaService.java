63341e9389b680f4cc3cfdd22c6d4b958d27151d
Eduardo Victor <eduardovictor28@gmail.com>
2022-05-25 14:09:24 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/eduardovictor/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.eduardovictor.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardovictor.cursomc.domain.Categoria;
+import com.eduardovictor.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
+	}
+}
