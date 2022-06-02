da151b18eb6d55b9abda11c064e0887624b51c7b
Eduardo Victor <eduardovictor28@gmail.com>
2022-06-02 17:11:50 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/eduardovictor/cursomc/CursomcApplication.java
+++ src/main/java/com/eduardovictor/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.eduardovictor.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.eduardovictor.cursomc.domain.Categoria;
+import com.eduardovictor.cursomc.domain.Produto;
 import com.eduardovictor.cursomc.repositories.CategoriaRepository;
+import com.eduardovictor.cursomc.repositories.ProdutoRepository;
 import com.eduardovictor.cursomc.services.CategoriaService;
 
 @SpringBootApplication
@@ -16,6 +19,8 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,7 +32,19 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));	
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
