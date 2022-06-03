ba48db7e323cbe2d5dcca7038a81ed53f9a11426
Eduardo Victor <eduardovictor28@gmail.com>
2022-06-03 18:03:52 -0300

    Ajuste no End point /categorias/{id}

--- src/main/java/com/eduardovictor/cursomc/services/CategoriaService.java
+++ src/main/java/com/eduardovictor/cursomc/services/CategoriaService.java
@@ -20,4 +20,5 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
 	}
+	
 }
