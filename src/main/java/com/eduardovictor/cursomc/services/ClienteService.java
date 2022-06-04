9073227286065c2437e3463c121fd8a9ce7c8c91
Eduardo Victor <eduardovictor28@gmail.com>
2022-06-04 10:38:31 -0300

    Endpoint /clientes/{id}

---
+++ src/main/java/com/eduardovictor/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.eduardovictor.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardovictor.cursomc.domain.Cliente;
+import com.eduardovictor.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName(), null));
+	}
+	
+}
