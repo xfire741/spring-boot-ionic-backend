1a8825a55b45d6f553cc996fecd8ba161b12c26a
Eduardo Victor <eduardovictor28@gmail.com>
2022-06-04 13:47:15 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/eduardovictor/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.eduardovictor.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardovictor.cursomc.domain.Pedido;
+import com.eduardovictor.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName(), null));
+	}
+	
+}
