34dad37691b57385c71abfd7154832feaadbc9b2
Eduardo Victor <eduardovictor28@gmail.com>
2022-06-04 11:40:21 -0300

    Pedido, EstadoPagamento, Pagamento

---
+++ src/main/java/com/eduardovictor/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.eduardovictor.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eduardovictor.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
