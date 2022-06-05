eef68eeed816dac60eaa43393d4bb9bfa22ff36f
Eduardo Victor <eduardovictor28@gmail.com>
2022-06-05 11:55:57 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/eduardovictor/cursomc/resources/exceptions/FieldMessage.java
@@ -0,0 +1,40 @@
+package com.eduardovictor.cursomc.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String fildName;
+	private String message;
+	
+	public FieldMessage() {
+	}
+
+	public FieldMessage(String fieldName, String message) {
+		super();
+		this.fildName = fildName;
+		this.message = message;
+	}
+
+	
+
+	public String getFildName() {
+		return fildName;
+	}
+
+	public void setFildName(String fildName) {
+		this.fildName = fildName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+}
