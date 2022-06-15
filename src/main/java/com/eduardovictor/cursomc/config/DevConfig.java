20a46e7657d8af95401b61e2d1c70e4a7eb1ba48
Eduardo Victor <eduardovictor28@gmail.com>
2022-06-15 12:15:43 -0300

    Implementando SmtpEmailService Google

--- src/main/java/com/eduardovictor/cursomc/config/DevConfig.java
+++ src/main/java/com/eduardovictor/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.eduardovictor.cursomc.services.DBService;
+import com.eduardovictor.cursomc.services.EmailService;
+import com.eduardovictor.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,5 +32,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
