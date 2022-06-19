8b3f0ef880ef89dbfcfcb517c87757e933f0c1e8
Eduardo Victor <eduardovictor28@gmail.com>
2022-06-19 10:02:40 -0300

    Implementando autorizacao

--- src/main/java/com/eduardovictor/cursomc/config/SecurityConfig.java
+++ src/main/java/com/eduardovictor/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.eduardovictor.cursomc.security.JWTAuthenticationFilter;
+import com.eduardovictor.cursomc.security.JWTAuthorizationFilter;
 import com.eduardovictor.cursomc.security.JWTUtil;
 
 @SuppressWarnings("deprecation")
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 			http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+			http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		}
 		
